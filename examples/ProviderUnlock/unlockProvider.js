8579fb8f897d3c1ce4f39634fdfd0f70d01600fb
Bohn-onOffice <61838147+Bohn-onOffice@users.noreply.github.com>
2022-07-15 15:58:10 +0200

    - added extendedclaim - remove unneeded functions in js because object is given with unlock - removed unnecessary comment lines

--- examples/ProviderUnlock/unlockProvider.js
+++ examples/ProviderUnlock/unlockProvider.js
@@ -1,4 +1,4 @@
-function unlockProvider(){
+function unlockProvider(inputParameters){
 
     var spinner = document.getElementById('spinner'),
         button = document.getElementById('unlockButton'),
@@ -7,10 +7,9 @@ function unlockProvider(){
         secretDiv = document.getElementById('secretDiv'),
         secret = secretTextInput.value;
 
-    var arguments = getArguments();
-
-    var token = arguments.getParameterToken,
-        parameterCacheId = arguments.getParameterCacheId;
+    var token = inputParameters.getParameterToken,
+        parameterCacheId = inputParameters.getParameterCacheId,
+        parameterExtendedClaim = inputParameters.getParameterApiClaim;
 
     spinner.style.visibility = 'visible';
     button.style.visibility = 'hidden';
@@ -21,6 +20,7 @@ function unlockProvider(){
         data.append('token', token);
         data.append('secret', secret);
         data.append('parameterCacheId', parameterCacheId);
+        data.append('extendedClaim', parameterExtendedClaim)
 
         var httpRequest = new XMLHttpRequest();
 		//An example of implementation can be found here https://github.com/onOfficeGmbH/sdk/blob/master/examples/03-marketplace-unlock-provider.php
@@ -47,32 +47,7 @@ function unlockProvider(){
     }, 1000);
 }
 
-function getArguments()
-{
-    arguments = {
-	"getParameterToken":getUrlParameter('apiToken'),
-	"getParameterCacheId":getUrlParameter('parameterCacheId'),
-	};
-
-    return arguments;
-}
-
 function updateParent(message){
     var target = window.parent;
     target.postMessage(message, '*');
-}
-
-//This function is only used to get the parameter from the url
-function getUrlParameter(sParam)
-{
-    var url = window.location.search.substring(1), urlvariables = url.split('&'), parameterName, i;
-
-    for (i = 0; i < urlvariables.length; i++)
-    {
-        parameterName = urlvariables[i].split('=');
-
-        if (parameterName[0] === sParam) {
-            return parameterName[1] === undefined ? true : decodeURIComponent(parameterName[1]);
-        }
-    }
 }
